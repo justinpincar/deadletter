9f66979832f167d0e7409b7550ff6a57be32b9d4
Justin Pincar <justinpincar@gmail.com>
2013-07-29 18:52:25 -0700

    Redirect to SSL if we're not on tor

--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
 module.exports = function(App) {
   return {
     root: function(req, res) {
+      if ((req.headers.host == "deadletter.io") && (!req.connection.encrypted)) {
+        return res.redirect('https://deadletter.io');
+      }
+
       return res.render('index');
     },
     notes: require('./routes/notes')(App)
